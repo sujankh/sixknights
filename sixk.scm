2aa6ecfa5bd43fcf81d562278e49ac58d37a1f5b
Sujan Khadka <sujank@uab.edu>
2016-04-27 15:05:31 -0500

    Functions to get valid postion to move to

--- sixk.scm
+++ sixk.scm
@@ -8,11 +8,63 @@
 ;;   )
 
 
+
+;;get the target position to which a knight could move
+;;the target position should not be where a knight already exists in the board
+;;also 
+(define getValidTargetPositions
+  (lambda (index filledPositions)
+    (delete-matching-items (getAllTargets index) (lambda(x) (isInvalidPosition x filledPositions)))
+    ))
+
+;;a position is invalid if
+;;another knight has already filled the position
+;;or as per our heuristic position = 4 or 7
+;;we don't want our knights to jump to 4 or 7
+(define isInvalidPosition
+  (lambda (pos filledPositions)
+    (or (eq? pos 4) (eq? pos 7) (member pos filledPositions))
+  ))
+
+;;returns all valid positions to which a knight could move
+;;assumes the board has only one knight
+(define getAllTargets
+  (lambda (index)
+    (let	
+	(
+	  (r (quotient index 3))
+	  (c (modulo index 3))	  
+	  )
+      (keep-matching-items
+       (map toindex
+       (list
+       (cons (- r 2) (+ c 1))
+       (cons (- r 2) (- c 1))
+       (cons (+ r 2) (+ c 1))
+       (cons (+ r 2) (- c 1))
+
+       (cons (+ r 1) (- c 2))
+       (cons (- r 1) (- c 2))
+       (cons (+ r 1) (+ c 2))
+       (cons (- r 1) (+ c 2))
+       ))
+       (lambda (x) (>= x 0))
+      ))
+     )
+  )
+
+;;get linear index from rowcol which is a list(r, c)
+ (define toindex
+  (lambda (rowcol)
+    (let ((r (car rowcol)) (c (cdr rowcol)))      
+      (if (and (>= r 0) (<= r 3) (>= c 0) (<= c 2))
+       (+ (* r 3) c) -1)
+      )))
+  
 ;; From the given list of positions remove the positions at which the knight is already at
 ;; its final position
 ;; eg. a Black knight at 10 , 11, 12 need not be moved
 ;; a white knight at 1, 2, 3 need not be moved
-
 ;; predicate is a lambda which is used to decide if a knight is in its final position or not
 (define removeFinalPositions
   (lambda (positions predicate)
@@ -39,15 +91,29 @@
     (<= position 2)
     ))
 
+(define isBlack
+  (lambda (symbol)
+    (eq? symbol 1)
+    ))
+
+(define isWhite
+  (lambda (symbol)
+    (eq? symbol 2)
+  ))
+
+
+;;(findKnights (lambda(x) (or (isWhite x) (isBlack x))) '(1 1 1 0 0 0 0 0 0 2 2 2) 0)
+
 ;;Returns the indices in the board at which the Knight is placed
 ;;eg {1, 1, 1, 0 ,0 ....} with symbol = 1 will return (0, 1, 2)
 (define findKnights
-  (lambda (symbol Board index)
+  (lambda (isKnight Board index)
      (cond
       ((null? Board) '())
-      ((eq? symbol (car Board))       	
-       (append (list index) (findKnights symbol (cdr Board) (+ index 1))))       
+      ;;((eq? symbol (car Board))
+      ((isKnight (car Board))       	
+       (append (list index) (findKnights isKnight (cdr Board) (+ index 1))))       
       (else
-       (append '() (findKnights symbol (cdr Board) (+ index 1))))
+       (append '() (findKnights isKnight (cdr Board) (+ index 1))))
       )    
   ))

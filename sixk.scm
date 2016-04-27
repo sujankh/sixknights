e5e9498f6310b4c6d97bb94dc7fcb07ede54b764
Sujan Khadka <sujank@uab.edu>
2016-04-27 12:08:45 -0500

    Filter the positions at which the knights are already at their final poisitions

--- sixk.scm
+++ sixk.scm
@@ -7,6 +7,38 @@
 ;;    )
 ;;   )
 
+
+;; From the given list of positions remove the positions at which the knight is already at
+;; its final position
+;; eg. a Black knight at 10 , 11, 12 need not be moved
+;; a white knight at 1, 2, 3 need not be moved
+
+;; predicate is a lambda which is used to decide if a knight is in its final position or not
+(define removeFinalPositions
+  (lambda (positions predicate)
+    (cond
+     ((null? positions) '())
+     ((predicate (car positions))
+      (append '() (removeFinalPositions (cdr positions) predicate)))
+     (else
+      (append
+       (list (car positions)) (removeFinalPositions (cdr positions) predicate))
+      )      
+     )
+  ))
+
+;;black is swapped if its position is >=9 (i.e. 9, 10, 11)
+(define blackSwapped
+  (lambda (position)
+    (>= position 9)
+    ))
+
+;;white is swapped if its position is <=2 (i.e. 0, 1, 2)
+(define whiteSwapped
+  (lambda (position)
+    (<= position 2)
+    ))
+
 ;;Returns the indices in the board at which the Knight is placed
 ;;eg {1, 1, 1, 0 ,0 ....} with symbol = 1 will return (0, 1, 2)
 (define findKnights
